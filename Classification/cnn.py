c0b5dd2ebc97864683e6d444e599defd53f06a38
Kenneth Tan <kennethtan@ucsb.edu>
2026-01-08 18:39:55 -0800

    Added dummy model file

---
+++ Classification/cnn.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self, in_channels: int, input_length: int, num_classes: int):
+        super.__init__()
+        #Write out necessary building blocks for CNN
+
+    def forward(self, x):
+        #Write out simple CNN structure
