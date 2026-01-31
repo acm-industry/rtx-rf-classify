55cfecc85092c3c2c2866e5f8345b3a39b1b4e22
Don Tran <75715248+dontran15@users.noreply.github.com>
2026-01-31 15:19:46 -0800

    formatting

--- Systems/src/vector/vector_ops.cpp
+++ Systems/src/vector/vector_ops.cpp
@@ -20,14 +20,12 @@ void dot(const float* a, const float* b, float* out, size_t n) {
     impl_dot(a, b, out, n);
 }
 
-void matmul(const float* a, const float* b, float* out,
-            size_t rows_a, size_t cols_a, size_t cols_b) {
+void matmul(const float* a, const float* b, float* out, size_t rows_a, size_t cols_a, size_t cols_b) {
     impl_matmul(a, b, out, rows_a, cols_a, cols_b);
 }
 
-void matvec(const float* a, const float* x, float* out,
-            size_t rows, size_t cols) {
+void matvec(const float* a, const float* x, float* out, size_t rows, size_t cols) {
     impl_matvec(a, x, out, rows, cols);
 }
 
-} // namespace vec
+}
