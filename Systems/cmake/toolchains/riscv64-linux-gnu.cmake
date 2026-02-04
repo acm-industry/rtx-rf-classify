6f08f7f6fa258edfc298ed4826d1e6f7f91480ba
Tanuj Siripurapu <tanujsiripurapu@gmail.com>
2026-02-03 18:23:17 -0800

    add missing cmake

---
+++ Systems/cmake/toolchains/riscv64-linux-gnu.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+set(CMAKE_C_COMPILER   riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
+
+set(RISCV_MARCH "rv64gc")
+set(RISCV_MABI  "lp64d")
+
+set(CMAKE_C_FLAGS_INIT   "-march=${RISCV_MARCH} -mabi=${RISCV_MABI}")
+set(CMAKE_CXX_FLAGS_INIT "-march=${RISCV_MARCH} -mabi=${RISCV_MABI}")
+
